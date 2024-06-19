66846b9ab9f177af2c670a9437792bdc0dc04f0d
Đoàn Đức Huy <doanhuy1103@gmail.com>
2024-06-19 17:27:23 +0700

    deploy

---
+++ daychuyen/daychuyen.js
@@ -0,0 +1,40 @@
+initQuantity();
+initFavorite();
+
+function initQuantity()
+	{
+		if($('.plus').length && $('.minus').length)
+		{
+			var plus = $('.plus');
+			var minus = $('.minus');
+			var value = $('#quantity_value');
+
+			plus.on('click', function()
+			{
+				var x = parseInt(value.text());
+				value.text(x + 1);
+			});
+
+			minus.on('click', function()
+			{
+				var x = parseInt(value.text());
+				if(x > 1)
+				{
+					value.text(x - 1);
+				}
+			});
+		}
+	}
+
+function initFavorite()
+	{
+		if($('.product_favorite').length)
+		{
+			var fav = $('.product_favorite');
+
+			fav.on('click', function()
+			{
+				fav.toggleClass('active');
+			});
+		}
+	}
